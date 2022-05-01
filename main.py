d1b577eada8ad98d41a0aace7a84a1a903ddced7
Lenni Hein <git@lennihein.com>
2022-05-01 18:23:11 +0200

    added another sample and some comfort feats.

--- main.py
+++ main.py
@@ -8,19 +8,33 @@ from src.predictor import Predictor
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-
     if len(args) == 0:
         rets = batch(file, "samples/")
         for i in rets:
             i.prediction = Predictor.strings_predict(i)
         falses = [i for i in rets if i.label!=None and i.prediction != i.label]
         trues = [i for i in rets if i.label!=None and i.prediction == i.label]
+        positives = [i for i in rets if i.prediction == True]
+        negatives = [i for i in rets if i.prediction == False]
+        true_pos = [i for i in positives if i.label == True]
+        true_neg = [i for i in negatives if i.label == False]
         print(file_table(rets), end="")
         if len(falses) > 0:
             print(f"\n{BOLD+WARNING}False predictions: {len(falses)}{ENDC}")
             print(file_table(falses))
         accuracy = len(trues)/(len(trues)+len(falses))
         print(f"\n{BOLD}Accuracy: {accuracy*100:2.0f}% ({len(trues)}/{len(trues)+len(falses)}){ENDC}")
+        print(f"Predicted {len(positives)} positives and {len(negatives)} negatives")
+
+        pos_needed = len(true_pos) <= len(true_neg)
+        print(f"\nHere is a random sample of the {'positives' if pos_needed else 'negatives'} without label:")
+        no_label = [i for i in (positives if pos_needed else negatives) if i.label is None]
+        import random
+        rand = random.choice(no_label)
+        print(file_table([rand]))
+        print(f"{BOLD}The following URLs are encrypted:{ENDC}")
+        for i in rand.encrypted_urls:
+            print(f"> {i}")
 
     else:
         f = file(args[0])
