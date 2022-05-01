126042b6623c079656f61c04a41e121c990d5b8a
Lenni Hein <git@lennihein.com>
2022-05-02 01:04:48 +0200

    done?

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 import sys
 from src.pretty_print import file_table, BOLD, WARNING, ENDC
-from src.tools import file, batch
+from src.tools import file, batch, sha256
 from src.predictor import Predictor
+import src.pe_parser as pe
 
 
 if __name__ == "__main__":
@@ -11,41 +12,95 @@ if __name__ == "__main__":
     if len(args) == 0:
         rets = batch(file, "samples/")
         for i in rets:
-            i.prediction = Predictor.strings_predict(i)
+            strings_predict = Predictor.strings_predict(i)
+            capstone_pred = Predictor.plpc(i)
+            i.prediction = capstone_pred and strings_predict
+
+            # check urls of predictions
+            # if i.prediction:
+            #     rand = i
+            #     print(file_table([rand], sha265_len=64))
+            #     if len(rand.urls) > 0:
+            #         print(f"{BOLD}The following URLs are unencrypted:{ENDC}")
+            #         for i in rand.urls:
+            #             print(f"> {i}")
+            #         print()
+            #     if len(rand.encrypted_urls) > 0:
+            #         print(f"{BOLD}The following URLs are encrypted:{ENDC}")
+            #         for i in rand.encrypted_urls:
+            #             print(f"> {i}")
+            #         print()
+
+            # # check lstrcpya importing, but not url encrypting
+            # if i.prediction == False and capstone_pred == True:
+            #     print(f"{WARNING}{i.sha256[:6]} | lstrcpya imported but NOT badnews {ENDC}")
+            #     rand = i
+            #     print(file_table([rand], sha265_len=64))
+            #     if len(rand.urls) > 0:
+            #         print(f"{BOLD}The following URLs are unencrypted:{ENDC}")
+            #         for i in rand.urls:
+            #             print(f"> {i}")
+            #         print()
+            #     if len(rand.encrypted_urls) > 0:
+            #         print(f"{BOLD}The following URLs are encrypted:{ENDC}")
+            #         for i in rand.encrypted_urls:
+            #             print(f"> {i}")
+            #         print()
+
+
         falses = [i for i in rets if i.label!=None and i.prediction != i.label]
         trues = [i for i in rets if i.label!=None and i.prediction == i.label]
         positives = [i for i in rets if i.prediction == True]
         negatives = [i for i in rets if i.prediction == False]
         true_pos = [i for i in positives if i.label == True]
         true_neg = [i for i in negatives if i.label == False]
-        print(file_table(rets), end="")
+        labeled = [i for i in rets if i.label!=None]
+
+        # print table
+        print(file_table(labeled), end="")
+
+        # print falsely predicted files
         if len(falses) > 0:
             print(f"\n{BOLD+WARNING}False predictions: {len(falses)}{ENDC}")
             print(file_table(falses))
+
+        # print stats regarding accuracy and predictions
         accuracy = len(trues)/(len(trues)+len(falses))
         print(f"\n{BOLD}Accuracy: {accuracy*100:2.0f}% ({len(trues)}/{len(trues)+len(falses)}){ENDC}")
         print(f"Predicted {len(positives)} positives and {len(negatives)} negatives")
 
-        pos_needed = len(true_pos) <= len(true_neg)
-        print(f"\nHere is a random sample of the {'positives' if pos_needed else 'negatives'} without label:")
-        no_label = [i for i in (positives if pos_needed else negatives) if i.label is None]
-        import random
-        rand = random.choice(no_label)
-        print(file_table([rand], sha265_len=64))
-        print(f"{BOLD}The following URLs are encrypted:{ENDC}")
-        for i in rand.encrypted_urls:
-            print(f"> {i}")
+        # # give suggestion for next analysis
+        # pos_needed = len(true_pos) <= len(true_neg)
+        # print(f"\nHere is a random sample of the {'positives' if pos_needed else 'negatives'} without label:")
+        # no_label = [i for i in (positives if pos_needed else negatives) if i.label is None]
+        # import random
+        # rand = random.choice(no_label)
+        # print(file_table([rand], sha265_len=64))
+        # if len(rand.urls) > 0:
+        #     print(f"{BOLD}The following URLs are unencrypted:{ENDC}")
+        #     for i in rand.urls:
+        #         print(f"> {i}")
+        #     print()
+        # if len(rand.encrypted_urls) > 0:
+        #     print(f"{BOLD}The following URLs are encrypted:{ENDC}")
+        #     for i in rand.encrypted_urls:
+        #         print(f"> {i}")
+        #     print()
 
     else:
         f = file(args[0])
-        f.prediction = Predictor.strings_predict(f)
+        strings_predict = Predictor.strings_predict(f)
+        capstone_pred = Predictor.plpc(f)
+        f.prediction = capstone_pred and strings_predict
         print(f)
         if len(f.urls) > 0:
             print(f"{BOLD}The following URLs are unencrypted:{ENDC}")
             for i in f.urls:
                 print(f"> {i}")
             print()
-        print(f"{BOLD}The following URLs are encrypted:{ENDC}")
-        for i in f.encrypted_urls:
-            print(f"> {i}")
-        print()
+        if len(f.encrypted_urls) > 0:
+            print(f"{BOLD}The following URLs are encrypted:{ENDC}")
+            for i in f.encrypted_urls:
+                print(f"> {i}")
+            print()
+        pe.get_file(args[0])
--- src/predictor.py
+++ src/predictor.py
@@ -1,4 +1,6 @@
 from src.file import FileInfo
+import pefile
+from capstone import *
 
 
 class Predictor():
@@ -8,4 +10,34 @@ class Predictor():
         return file
 
     def strings_predict(file: FileInfo) -> bool:
+        # so far none of the delphi GUI applications have been badnews samples
+        if file.GUI:
+            for i in file.encrypted_urls:
+                if "xml" in i:
+                    return True
+            return False
+        # if at least 2 encrypted urls exist, it's probably a badnews sample
         return True if len(file.encrypted_urls) >= 2 else False
+
+
+    def plpc(file: FileInfo) -> bool:
+
+        pe = pefile.PE(file.path)
+        entryPoint = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+        data = pe.get_memory_mapped_image()[entryPoint:]
+        cs = Cs(CS_ARCH_X86, CS_MODE_32)
+        instructions = cs.disasm(data, 0x1000)
+        instructions = list(instructions)
+
+        try:
+            lstrcpyA = None
+            for entry in pe.DIRECTORY_ENTRY_IMPORT:
+                dll_name = entry.dll.decode('utf-8')
+                if dll_name == "KERNEL32.dll":
+                    for func in entry.imports:
+                        if func.name.decode('utf-8') == 'lstrcpyA':
+                            lstrcpyA = func.address
+                            return True
+        except AttributeError:
+            return False
+        return False
